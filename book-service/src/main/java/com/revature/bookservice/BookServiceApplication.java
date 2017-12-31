82de81e8dc43790df3b8da3f6c4aba650cad8642
August Duet <august.duet@revature.com>
2017-12-30 19:21:44 -0500

    add book list endpoint

--- book-service/src/main/java/com/revature/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/com/revature/bookservice/BookServiceApplication.java
@@ -2,11 +2,25 @@ package com.revature.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
+@RestController
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
+	}
+
+	@RequestMapping(value="/books", method= RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity getBookList() {
+		return new ResponseEntity<Object>(new ArrayList<Object>(0), HttpStatus.OK);
 	}
 }
